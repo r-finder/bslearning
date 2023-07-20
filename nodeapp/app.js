72a800451720e6d24cc45f0c01c06b2d21251f44
Alex S <alex.sustavov@outlook.com>
2023-07-20 17:55:37 +0200

    added express

---
+++ nodeapp/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => {
+  res.send('Hello xxx World!')
+})
+
+app.get('/home', (req, res) => {
+  res.send('Welcom home!')
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
